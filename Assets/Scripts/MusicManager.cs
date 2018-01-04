a5b3c311ba11f857450e32d5d89abdafaf87b600
Andres Herrera <arhz29@gmail.com>
2018-01-04 16:41:47 -0500

    Music Manager

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+	public AudioClip[] levelMusicArray;
+
+	private void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+		SceneManager.sceneLoaded += OnLevelLoaded;
+	}
+	private void OnLevelLoaded(Scene scene, LoadSceneMode mode)
+	{
+		if (levelMusicArray[scene.buildIndex])
+		{
+			GetComponent<AudioSource>().clip = levelMusicArray[scene.buildIndex];
+			GetComponent<AudioSource>().loop = true;
+			GetComponent<AudioSource>().Play();
+		}
+	}
+}
